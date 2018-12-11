1937637fcd9804668c00bd1c4fef4e0b90e9fd0f
lagiovi <45786612+lagiovi@users.noreply.github.com>
2018-12-11 10:43:17 +0000

    Add files via upload

---
+++ bott/register.php
@@ -0,0 +1,24 @@
+		<?php
+		//https://api.telegram.org/bot118569545:AAFlvM1DcIhbs63TlnF_bBFmEZ4Vni2inyQ/setwebhook?url=https://siabot.herokuapp.com/execute.php
+		// PARAMETRI DA MODIFICARE
+		//inserite vostro indirizzo su heroku 
+		//https://username.herokuapp.com/execute.php
+		$WEBHOOK_URL = 'https://testgiovanna.herokuapp.com/execute.php';
+		//inserite il vostro token che vi ha fornito l'utente telegram @botfather
+		$BOT_TOKEN = '722596951:AAGI77l915t8BgMd9Ka6XlSs0eulqW2KqTE';
+
+		// NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
+		$API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
+		$method = 'setWebhook';
+		$parameters = array('url' => $WEBHOOK_URL);
+		$url = $API_URL . $method. '?' . http_build_query($parameters);
+		
+		$handle = curl_init($url);
+
+		
+		curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
+		curl_setopt($handle, CURLOPT_TIMEOUT, 60);
+		$result = curl_exec($handle);
+		print_r($result);
+		?>
